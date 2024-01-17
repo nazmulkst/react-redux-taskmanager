981654385a23d43e8db6ca0fa93f603b5ae45fa4
nazmulkst <nazmulkst@gmail.com>
2024-01-17 23:42:30 +0600

    React Mofify Comment

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,37 @@
-import { useState } from 'react'
+import { Fragment, useState } from 'react'
+import DashboardPage from './pages/Dashboard-Page';
+import CreatePage from './pages/Create-Page';
+import NewPage from './pages/New-Page';
+import ProgressPage from './pages/Progress-Page';
+import CompletedPage from './pages/Completed-Page';
+import CancelPage from './pages/Cancel-Page';
+import ProfilePage from './pages/Profile-Page';
+import LoginPage from './pages/Login-Page';
+import RegistrationPage from './pages/Registration-Page';
+import Page404 from './pages/Page-404';
+import ForgetPasswordPage from './pages/ForgetPassword-Page';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1>Hello this is running from React (Vite)</h1>
-        
+      <Fragment>
+        <BrowserRouter>
+          <Routes>
+            <Route exact path="/" element={<DashboardPage/>} />
+            <Route exact path="/Create" element={<CreatePage/>} />
+            <Route exact path="/All" element={<NewPage/>} />
+            <Route exact path="/Progress" element={<ProgressPage/>} />
+            <Route exact path="/Completed" element={<CompletedPage/>} />
+            <Route exact path="/Cancel" element={<CancelPage/>} />
+            <Route exact path="/Profile" element={<ProfilePage/>} />
+            <Route exact path="/Login" element={<LoginPage/>} />
+            <Route exact path="/Registration" element={<RegistrationPage/>} />
+            <Route exact path="/ForgetPassword" element={<ForgetPasswordPage/>} />
+            <Route path="*" element={<Page404/>} />
+          </Routes>
+        </BrowserRouter>
+      </Fragment>
     </>
   )
 }
